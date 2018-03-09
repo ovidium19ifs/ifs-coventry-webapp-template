fa31c89f7d2981b6f5cb9657ad4b64eee14767f2
ovidium19 <mitroio@uni.coventry.ac.uk>
2018-03-09 10:43:58 +0000

    collated all data files into a singular one(will make saving data easier)

--- server/partsController.js
+++ server/partsController.js
@@ -9,48 +9,26 @@ module.exports.get = function(req,res){
     var path = req.params.source;
     var partialPath="resourcesfor"+path+"/";
     var fullPath = "data/"+partialPath;
+    //only 1 file -- data.json
     try{
-        var files=fs.readdirSync(fullPath);
-        files.sort();
+        var results = fs.readFileSync(fullPath+"/data.json");
     }
-    catch(e){
-
+    catch (e){
         res.end();
+        throw e;
     }
-    var results="[";
-    for (var i=0;i<files.length;i++){
-        console.log(files[i]);
-        if (files[i].indexOf(".json")===files[i].length-5){
-
-            var fileContents = fs.readFileSync(fullPath+"/"+files[i]);
-            results+=fileContents+",";
-        }
-    }
-    results = results.substr(0,results.length-1);
-    results+="]";
-
     res.setHeader("Content-Type","application/json");
-
     res.send(results);
     res.end();
 };
 module.exports.put = function(req,res){
     "use strict";
-    var path = "data/resourcesfor"+toLowerCase(req.params.source)+"/";
-    var prefix;
-    var fileName;
-    for (var i=0;i<=req.body.length-1;i++){
-        prefix = i<9 ? "0"+(i+1) : i+1;
-        fileName = path+prefix+"_"+req.body[i].name.replace(/\s/g,"_")+".json";
-
-        try{
-            console.log("Writing to " + fileName);
-            fs.writeFileSync(fileName,JSON.stringify(req.body[i],null,4));
-        }
-        catch(e){
-
-            res.end();
-        }
+    var path = "data/resourcesfor"+toLowerCase(req.params.source)+"/data.json";
+    try{
+        fs.writeFileSync(path,JSON.stringify(req.body,null,4));
+    }
+    catch (e){
+        throw e;
     }
     res.setHeader("Content-Type","application/json");
     res.send({'response':"200"});
