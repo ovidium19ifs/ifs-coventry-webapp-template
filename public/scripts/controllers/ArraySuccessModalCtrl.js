2fc0071f8e3b5bab10e5ca8bffe6f8793f97204b
ovidium19 <mitroio@uni.coventry.ac.uk>
2018-05-16 11:41:15 +0100

    Added download data button to success modal

--- public/scripts/controllers/ArraySuccessModalCtrl.js
+++ public/scripts/controllers/ArraySuccessModalCtrl.js
@@ -2,15 +2,20 @@ module.exports = function(application){
     "use strict";
     //this uses angular ui bootstrap modal component
     //need to inject $uibModalInstance to properly close the modal instance
-    application.controller("ArraySuccessModalCtrl",["$scope","$uibModalInstance",
-        function($scope,$uibModalInstance){
+    application.controller("ArraySuccessModalCtrl",["$scope","$uibModalInstance","$location","$window",
+        function($scope,$uibModalInstance,$location,$window){
             "use strict";
-           
+           $scope.downloaded = false;
             $scope.closeModal = function(){
                 $uibModalInstance.close({val: true});
             };
             $scope.dismissModal = function(){
                 $uibModalInstance.dismiss("Modal dismissed");
-            }
+            };
+          $scope.downloadData = function () {
+            let download_url = $location.absUrl().replace($location.url(),"/download");
+            $window.open(download_url+"?auth=true");
+            $scope.downloaded=true;
+          };
         }]);
 };
